a6931a87231cebbdf4d624b3f8266436137f0b28
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-01-16 20:04:34 +0200

    Imitate WebClient

---
+++ src/test/java/com/webflux/webfluxdemo/webclient/Config/WebClientConfig.java
@@ -0,0 +1,19 @@
+package com.webflux.webfluxdemo.webclient.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+/**
+ * Imitate WebClient
+ */
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl("http://localhost:8080")
+                .build();
+    }
+}
