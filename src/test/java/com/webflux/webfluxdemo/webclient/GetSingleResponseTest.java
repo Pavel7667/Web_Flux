261dac68df8df113a8bf57008c4b08bcbe852528
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-01-16 23:02:06 +0200

    StepVerifier Mono

--- src/test/java/com/webflux/webfluxdemo/webclient/GetSingleResponseTest.java
+++ src/test/java/com/webflux/webfluxdemo/webclient/GetSingleResponseTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Configuration
 @Slf4j
@@ -23,6 +25,18 @@ public class GetSingleResponseTest extends BaseTest {
                 .block();
 
         log.info(String.valueOf(response));
+    }
+
+    @Test
+    public void setVerifierTest() {
+        Mono<Response> responseMono = this.webClient.get()
+                .uri("reactive_math/square/{input}", 5)
+                .retrieve()
+                .bodyToMono(Response.class);
+
+        StepVerifier.create(responseMono)
+                .expectNextMatches(r -> r.getOutput() == 26)
+                .verifyComplete();
     }
 
 }
